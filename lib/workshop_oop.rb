2f0e5ffe59ca4fc5384d2d8b4626458017afe919
Sergey Belov <belovtorzhok@gmail.com>
2019-02-13 10:40:40 +0300

    first commit

---
+++ lib/workshop_oop.rb
@@ -0,0 +1,6 @@
+require "workshop_oop/version"
+
+module WorkshopOop
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/workshop_oop/version.rb
@@ -0,0 +1,3 @@
+module WorkshopOop
+  VERSION = "0.1.0"
+end
