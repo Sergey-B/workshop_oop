e065d980fad33af3c1fee3eb703ee2c3f9b4edde
Sergey Belov <belovtorzhok@gmail.com>
2019-02-16 09:42:00 +0300

    add base functionality

--- lib/workshop_oop.rb
+++ lib/workshop_oop.rb
@@ -1,6 +1,24 @@
 require "workshop_oop/version"
+require "open-uri"
+require "json"
+require "ostruct"
 
 module WorkshopOop
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.make_geo_request
+    -> ip_address = nil {
+      uri = URI.parse("http://ip-api.com/json/#{ip_address}")
+      uri.read
+    }
+  end
+
+  def self.parse_json
+    -> json { JSON.parse json, symbolize_names: true }
+  end
+
+  def self.get_geo(ip_address = nil, geo_service: make_geo_request, parser: parse_json)
+    response = geo_service.call(ip_address)
+    parser.call(response)
+  end
 end
--- spec/workshop_oop_spec.rb
+++ spec/workshop_oop_spec.rb
@@ -1,9 +1,40 @@
+require "workshop_oop"
+
 RSpec.describe WorkshopOop do
+  let(:ip_address) { "134.234.3.2" }
+  let(:geo_data) {
+    {
+      as: "AS1586 DoD Network Information Center",
+      city: "Sierra Vista",
+      country: "United States",
+      countryCode: "US",
+      isp: "DoD Network Information Center",
+      lat: 31.5552,
+      lon: -110.35,
+      org: "USAISC",
+      query: "134.234.3.2",
+      region: "AZ",
+      regionName: "Arizona",
+      status: "success",
+      timezone: "America/Phoenix",
+      zip: "85613"
+    }
+  }
+  let(:geo_service) {
+    double("GeoService", call: geo_data.to_json)
+  }
+
   it "has a version number" do
     expect(WorkshopOop::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "returns geo data" do
+    expect(WorkshopOop.get_geo(ip_address, geo_service: geo_service)).to eq geo_data
+  end
+
+  context "when ip_address is absent" do
+    it "returns self ip geo data" do
+      expect(WorkshopOop.get_geo(geo_service: geo_service)).to eq geo_data
+    end
   end
 end
